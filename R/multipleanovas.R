51524c7b6cc11faccb3de421cbeead629f9b1159
TimBock <tim.bock@numbers.net.au>
2016-10-25 11:13:38 +1100

    CE-170 TableOfMeans

--- R/multipleanovas.R
+++ R/multipleanovas.R
@@ -10,7 +10,7 @@
 #'   name of a variable in \code{data}. It may not be an expression.
 #' @param compare One of \code{"To mean", "Pairwise", "To first"} (which implement's Dunnett's C, when
 #' combined with 'correction' == 'Tukey Range'), or \code{"All"}
-#' @param correction The multiple comparison adjustment method: \code{"Tukey Range", "None",
+#' @param correction The multiple comparison adjustment method: \code{"Table FDR", "Tukey Range", "None",
 #' "False Discovery Rate", "Benjamini & Yekutieli", "Bonferroni",
 #' "Free Combinations"}, \code{"Hochberg", "Holm",
 #' "Hommel", "Single-step"} \code{"Shaffer"}, and \code{"Westfall"}.
@@ -29,8 +29,9 @@
 #' @param p.cutoff The alpha level to be used in testing.
 #' @param seed The random number seed used when evaluating the multivariate t-distribution.
 #' @param ... Other parameters to be passed to \code{OneWayANOVA}.
-#' @details Conducts multiple \code{OneWayANOVA}s, and puts them in a list.
-#' Additional detail about the other parameters can be found in \code{OneWayANOVA}.
+#' @details Conducts multiple \code{OneWayANOVA}s, and puts them in a list. If \code{correction} is
+#'  \code{"Table FDR"}, the false discovery rate correction is applied across the entire table. All
+#' other corrections are performed within rows. Additional detail about the other parameters can be found in \code{OneWayANOVA}.
 #' @importFrom flipFormat Labels
 #' @export
 MultipleANOVAs <- function(dependents,
@@ -38,7 +39,7 @@ MultipleANOVAs <- function(dependents,
                            subset = NULL,
                            weights = NULL,
                            compare = "To mean",
-                           correction = "Tukey Range",
+                           correction = "Table FDR",
                            robust.se = FALSE,
                            alternative = "Two-sided",
                            missing = "Exclude cases with missing data",
@@ -47,17 +48,34 @@ MultipleANOVAs <- function(dependents,
                            p.cutoff = 0.05,
                            ...)
 {
-    anovas <- lapply(dependents, function(x) OneWayANOVA(x,
+    anovas <- suppressWarnings(lapply(dependents, function(x) OneWayANOVA(x,
                                                        independent,
                                                        compare = "To mean",
                                                        subset = subset,
                                                        weights = weights,
-                                                       correction = correction,
-                                                       alternative = alternative,
-                                                       p.cutoff = p.cutoff,
-                                                       seed = seed,
-                                                       all.results = TRUE,
-                                                       ...))
+                                                       correction = if(correction == "Table FDR") "None" else correction,
+                                                       #alternative = alternative,
+                                                       #p.cutoff = p.cutoff,
+                                                       #seed = seed,
+                                                       #return.all = TRUE,
+                                                       ...)))
+    # Performing FDR correction.
+    ps <- unlist(lapply(anovas, function(x) x$coefs[, 4]))
+    if (correction == "Table FDR")
+    {
+        n.anovas <- length(anovas)
+        ps <- p.adjust(ps, method = "fdr")
+        counter <- 1
+        for (a in 1:n.anovas)
+        {
+            k <- nrow(anovas[[a]]$coefs)
+            var.ps <- ps[counter:(counter + k - 1)]
+            anovas[[a]]$coefs[, 4] <- var.ps
+            anovas[[a]]$p <- min(var.ps)
+            counter <- counter + k
+        }
+    }
+    attr(anovas, "ps") <- ps
     names(anovas) <- flipFormat::Labels(dependents)
     anovas
 }
@@ -91,6 +109,7 @@ FormattableANOVAs <- function(anovas, title, subtitle, footer)
 #' \code{ANOVAsAsTable}
 #' Converts a list of ANOVAs into a format that can be prettily formatted.
 #' @param x The list of ANOVAs.
+#' @importFrom flipTables Rbind
 #' @export
 ANOVAsAsTable <- function(x)
 {
@@ -102,16 +121,16 @@ ANOVAsAsTable <- function(x)
     for (i in x)
     {
         coefs <- i$coefs
-        means <- rbind(means, coefs[, 1])
-        zs <- rbind(zs, coefs[, 3])
-        ps <- rbind(ps, coefs[, 4])
+        means <- Rbind(means, coefs[, 1])
+        zs <- Rbind(zs, coefs[, 3])
+        ps <- Rbind(ps, coefs[, 4])
         r.squared <- c(r.squared, i$r.squared)
         overall.p <- c(overall.p, i$p)
     }
     rownames(means) <- names(x)
     group.names <- x[[1]]$column.names
     n <- x[[1]]$n
-    column.names <- paste0(group.names, "<br>","n: ", n)
+    column.names <- paste0(group.names, "<br>", "n: ", n)
     colnames(means) <- LETTERS[1:(k <- ncol(means))]
     colnames(ps) <- paste0(LETTERS[1:k], "1")
     return(list(means = means,
