8661ef9140ea1a950cb4e1a7056b74764f75a6d1
chschan <chschan@users.noreply.github.com>
2020-03-20 09:43:56 +1100

    DS-2831: Fix formatting error in last commit

--- R/segmentcomparisontable.R
+++ R/segmentcomparisontable.R
@@ -194,7 +194,8 @@ SegmentComparisonTable <- function(x, group, weights = NULL, subset = TRUE,
     }
     rownames(result) <- NULL # result is preserved in numeric form for exporting
     result.formatted <- matrix("", nrow(result), ncol(result))
-    for (i in 1:nrow(result))
+    result.formatted[1,] <- formatC(result[1,], 0, format = "f", big.mark = ",") # first row is always unweighted sample size
+    for (i in 2:nrow(result))
         result.formatted[i,] <- if (!show.index.values && row.format[i] == "numeric") formatC(result[i,], if (i == 1) 0 else format.numeric.decimals, format = "f", big.mark = ",")
                                 else                            paste0(formatC(result[i,] * 100, format.percentage.decimals, format = "f", big.mark = ","), "%")
     result.formatted[!is.finite(result)] <- ""
